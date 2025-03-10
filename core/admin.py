5368eb16187d9898e6cb7cdf84283a02ad2b8d16
Kudrat0777 <114284502+AnnSkachkova@users.noreply.github.com>
2025-03-10 21:37:33 +0300

    Minor amendments

--- core/admin.py
+++ core/admin.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
-from unfold.admin import ModelAdmin
+# from unfold.admin import ModelAdmin
 from .models import Employee, About, Contact
 
 # Register your models here.
 
-@admin.register(Employee)
-class EmployeeAdmin(ModelAdmin) :
-    list_display = ["image", "full_name", "role"]
-    list_display_links = ["image", "full_name"]
+# @admin.register(Employee)
+# @admin.register(Employee)
+# class EmployeeAdmin(ModelAdmin) :
+#     list_display = ["image", "full_name", "role"]
+#     list_display_links = ["image", "full_name"]
     
-@admin.register(About) 
-class AboutAdmin(ModelAdmin) :
-    list_display = ['title', "subtitle"]
-    list_display_links = ['title', "subtitle"]
+# @admin.register(About) 
+# class AboutAdmin(ModelAdmin) :
+#     list_display = ['title', "subtitle"]
+#     list_display_links = ['title', "subtitle"]
     
-@admin.register(Contact)
-class ContactAdmin(ModelAdmin) :
-    list_display = ['phone', "email", "location"]
-    list_display_links = ['phone', "email", "location"]
+# @admin.register(Contact)
+# class ContactAdmin(ModelAdmin) :
+#     list_display = ['phone', "email", "location"]
+#     list_display_links = ['phone', "email", "location"]
--- main/settings.py
+++ main/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "unfold",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "core",
+    # "unfold",
+    
+    
     
 ]
 
